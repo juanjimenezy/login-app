6e659e309772c76b3d6f9ed12c722fdbcb7b0096
Juan Jimenez <juanjiimenez8@gmail.com>
2022-11-03 13:06:17 -0500

    router

--- src/App.js
+++ src/App.js
@@ -8,21 +8,21 @@ import Register from './components/Register';
 import Home from './components/Home';
 import Detalle from './components/CrudComponent/detalle';
 
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 
 function App() {
 
   return (
     <>
-      <HashRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<><Header /><Home /></>}></Route>
           <Route path="/Login" element={<><Header /><Login /></>}></Route>
           <Route path="/Register" element={<><Header /><Register /></>}></Route>
           <Route path="/Crud" element={<><Header /><Detalle /></>}></Route>
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </>
   );
 }
