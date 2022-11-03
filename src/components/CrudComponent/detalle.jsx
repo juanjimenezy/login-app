730b965fe05f86b1c09b59ac1934b1e88120362f
Juan Jimenez <juanjiimenez8@gmail.com>
2022-11-03 11:57:13 -0500

    crud

---
+++ src/components/CrudComponent/detalle.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Detalle() {
+
+    return(
+        <>
+            <h1>Detalle Crud</h1>
+        </>
+    );
+
+}
+
+
+export default Detalle;
---
+++ src/firebaseConfig.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBmUOLf3ddmVVIOF-i_dyd3DRD83rClzyo",
+  authDomain: "login-app-7ebef.firebaseapp.com",
+  projectId: "login-app-7ebef",
+  storageBucket: "login-app-7ebef.appspot.com",
+  messagingSenderId: "409504685485",
+  appId: "1:409504685485:web:7ab396e8553c395ff6d524",
+  measurementId: "G-NQ91974281"
+};
+
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
