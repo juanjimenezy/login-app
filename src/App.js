9514f97a11072f42c0c423557916a5c9afd5499c
Juan Jimenez <juanjiimenez8@gmail.com>
2022-11-04 13:42:01 -0500

    heroku

--- src/App.js
+++ src/App.js
@@ -8,40 +8,39 @@ import Register from './components/Register';
 import Home from './components/Home';
 import Detalle from './components/CrudComponent/detalle';
 
-import {  createBrowserRouter,  RouterProvider,  Route,} from "react-router-dom";
+import {  createBrowserRouter,  RouterProvider,  Route,Routes} from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <><Header /> <Home/></>,
-  },
-  {
-    path: "/Login",
-    element: <><Header /> <Login/></>,
-  },
-  {
-    path: "/Register",
-    element: <><Header /> <Register/></>,
-  },
-  {
-    path: "/Crud",
-    element: <><Header /> <Detalle/></>,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <><Header /> <Home/></>,
+//   },
+//   {
+//     path: "/Login",
+//     element: <><Header /> <Login/></>,
+//   },
+//   {
+//     path: "/Register",
+//     element: <><Header /> <Register/></>,
+//   },
+//   {
+//     path: "/Crud",
+//     element: <><Header /> <Detalle/></>,
+//   },
+// ]);
 
 
 function App() {
 
   return (
-    // <>
-    //     <Routes>
-    //       <Route path="/" element={<><Header /> <Home/></>}>a</Route>
-    //       <Route path="/Login" element={<Login/>}></Route>
-    //       <Route path="/Register" element={<Register/>}>c</Route>
-    //       <Route path="/Crud" element={<Detalle />}>d</Route>
-    //     </Routes>
-    // </>
-    <RouterProvider router={router} />
+    <>
+        <Routes>
+          <Route path="/" element={<><Header/>  <Home/></>}></Route>
+          <Route path="/Login" element={<><Header/>  <Login/></>}></Route>
+          <Route path="/Register" element={<><Header/>  <Register/></>}></Route>
+          <Route path="/Crud" element={ <><Header/>  <Detalle /></>}></Route>
+        </Routes>
+    </>
   );
 }
 
